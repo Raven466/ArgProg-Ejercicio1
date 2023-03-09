2d5ad3abf1921b370d0b650a5e36f934d284db19
Diego_C <diego91.466@gmail.com>
2023-03-09 15:59:09 -0300

    Agrego bolleano para saber en que ejercicio estoy

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         // 1. a) Utilizando la sentencia while, imprima todos los números entre 2 variables “a” y “b”.
+        boolean estoyEnElEjercicio1 = true;
         int numeroInicio = 5;
         int numeroFin = 14;
         while(numeroInicio <= numeroFin) {
